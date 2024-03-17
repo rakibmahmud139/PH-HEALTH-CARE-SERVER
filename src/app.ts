a1e88ee63c292659ee76d58db46db9f96ad0bbbc
Rakibmahmud <hasansaikat74@gmail.com>
2024-03-17 06:41:02 +0600

    created admin

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { userRoutes } from "./app/modules/user/user.routes";
 
 const app: Application = express();
 
 //middleware
-app.use(express.json());
 app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
     Message: "Ph health care server...",
   });
 });
+
+app.use("/api/v1/user", userRoutes);
 
 export default app;
